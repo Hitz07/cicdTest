e771a401d0861afedd7bfbcc651c91d0923b27ba
HV <hiteshv77@gmail.com>
2025-07-16 00:35:31 +0530

    updated UI TC

--- ui_tests/test_google_search.py
+++ ui_tests/test_google_search.py
@@ -1,13 +1,21 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.options import Options
 
-def test_google_search():
-    driver = webdriver.Chrome()
-    driver.get("https://www.google.com")
+def test_duckduckgo_search():
+    options = Options()
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://duckduckgo.com")
     search = driver.find_element(By.NAME, "q")
     search.send_keys("XO Health QA")
     search.submit()
-    time.sleep(2)
+    time.sleep(3)
+
     assert "XO Health" in driver.page_source
+
     driver.quit()
